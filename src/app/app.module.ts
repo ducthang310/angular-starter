9f00963b8213faec48629c9631108cde7ca90711
Thang <ducthang310@gmail.com>
2020-07-30 18:10:43 +0700

    Auth guard, error handler, configuration for api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
 import { JwtModule } from '@auth0/angular-jwt';
 import { TOKEN_NAME } from './core/constants';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './app-components/layout/index/index.component';
+import { AppErrorHandler } from './core/app.error-handler';
+import { AuthInterceptor } from './core/auth/auth.interceptor';
+import { AuthGuard } from './core/auth/auth.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, IndexComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +33,18 @@ import { TOKEN_NAME } from './core/constants';
       }
     }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: AppErrorHandler
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    AuthGuard, AuthInterceptor
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
