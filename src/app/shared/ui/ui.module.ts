d6c1308980cd8df45e843a1a4616fee61936f242
Thang <ducthang310@gmail.com>
2020-07-30 17:02:28 +0700

    Booking note: show loading mask after submitting

--- src/app/shared/ui/ui.module.ts
+++ src/app/shared/ui/ui.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalConfirmComponent } from './modal-confirm/modal-confirm.component';
+import { LoadingComponent } from './loading/loading.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ModalConfirmComponent, LoadingComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ModalConfirmComponent, LoadingComponent
   ]
 })
 export class UiModule { }
